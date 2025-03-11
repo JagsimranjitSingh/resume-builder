46f1a31f90f81bd911e919be73a0dd0b3336318d
JagsimranjitSingh <jaskainth38@gmail.com>
2025-03-11 21:09:13 +0530

    Add KINDE authentication

--- app/(landingPage)/page.tsx
+++ app/(landingPage)/page.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
+import { RegisterLink, LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
 
 export default function Home() {
   return (
-    <div>Welcome</div>
+    <div>
+      <LoginLink>Sign in</LoginLink>
+      <RegisterLink>Sign up</RegisterLink>
+    </div>
   );
 }

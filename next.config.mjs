90b4908ebd6ea40b681357db0cadce0a24ee8a6b
JagsimranjitSingh <jaskainth38@gmail.com>
2025-04-05 16:52:48 +0530

    Add 'Create Resume' Feature

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
-/** @type {import('next').NextConfig} */
-import dotenv from 'dotenv';
+/** @type {import('next').NextConfig} **/
+import dotenv from "dotenv";
 dotenv.config();
 const nextConfig = {};
 
